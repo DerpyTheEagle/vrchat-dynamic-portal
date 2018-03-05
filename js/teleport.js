af2130232e110730ecc5fd97c5423208a6b489d8
Connor Leighton <connor.leighton@shaw.ca>
2018-03-05 16:17:35 -0700

    Commented stuff and made small improvements.

--- js/teleport.js
+++ js/teleport.js
@@ -17,18 +17,45 @@ function onBindingsReady(evt) {
 }
 document.addEventListener('onBindingsReady', onBindingsReady, false);
 
-function addWorld(worldimage, worldtitle, worldid) {
-  var template;
-  var element;
-  template = "<li><table class=\"world\" onClick=\"goToWorld(\'" + worldid + "\');\"><tr><th style=\"width:17%\"><img class=\"worldpreview\" src=\"" + worldimage + "\"></th><th style=\"width:83%\"><h1 class=\"worldtitle\">" + worldtitle + "</h1></th><tr></li>";
+// Something doesn't play nice with the $ selector, so we need noConflict
+jQuery.noConflict();
 
-  element = document.createElement('ul');
+// we can't see the console.log in game, so let's use our own function for output!
+function debug(text) {
+  console.log(text);
+  // append our output to the dev console
+  jQuery("#devconsole").append("<p>" + text + "</p>");
+}
+
+// if anything breaks, let's print it to our own homebrew console
+window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+  debug("ERROR: " + errorMsg + " on line " + lineNumber + " in file " + url);
+  return false;
+}
+
+// notify the user when the page has finished loading, then populate the worlds list
+document.addEventListener("DOMContentLoaded", function() {
+  debug("page successfully loaded!");
+  // call the populate function
+  populateWorlds();
+});
+
+function addWorld(worldimage, worldtitle, worldid) {
+  /*
+  this is the template to display a world!
+  I would have used a template literal, but the VRChat JS engine doesn't like them, so old style it is
+  */
+  var template = "<table class=\"world\" onClick=\"goToWorld(\'" + worldid + "\');\"><tr><th><img class=\"worldpreview\" src=\"" + worldimage + "\"></th><th style=\"width:100%;padding-left:30px;\"><h1 class=\"worldtitle\">" + worldtitle + "</h1></th><tr>";
+  // create a new list element
+  var element = document.createElement('li');
+  // make the innerHTML of our new list element the previously filled out template, then append it to the list
   element.innerHTML = template;
-  jQuery("#container").append(element);
+  jQuery("#worldlist").append(element);
 }
 
 function populateWorlds() {
   debug("populating worlds list!");
+  // for every entry in the worlds list, call addWorld to append it
   for (i = 0; i < worlds.length; i++) {
     addWorld(worlds[i].image, worlds[i].title, worlds[i].id);
   }
@@ -36,22 +63,35 @@ function populateWorlds() {
 
 // borrowed from naqtn on GitHub
 function goToWorld(id) {
+  /*
+  if the bindings aren't ready, that means that:
+  1. we tried to port before the engine has had time to load (unlikely, but possible)
+  2. the engine isn't available at all and we're not in VRChat
+  */
   if (!bindingsReady) {
+    // so if we haven't got the bindings, warn in the debug console...
     debug("tried to port, but bindings aren't ready! are we even in VRChat?");
+    /// and break out of the function
     return;
   }
+  // otherwise, output to the dev console about a (supposed) success
   debug("jumping to world= " + id);
+  // and then call the engien function
   engine.call("VRCSDK2.Networking.GoToRoom", id);
 }
 
+// turn the dev console on
 function onDevConsole() {
   jQuery("#devconsole").removeClass("hidden");
 }
 
+// turn the dev console off
 function offDevConsole() {
   jQuery("#devconsole").addClass("hidden");
 }
 
+// evaluate the dev console's input for debugging
 function evalDevConsole() {
   eval(jQuery("#devconsoleinput").val());
+  jQuery("#devconsoleinput").val('');
 }
